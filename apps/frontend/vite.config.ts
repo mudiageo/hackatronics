c3527dd5a15ed02b0da00bad4cb91865dc037c40
Mudia Geo <mudiageo@gmail.com>
2026-09-17 10:58:13 +0000

    add frontend

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+import { lazyPlugins } from "vite-plus";
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: lazyPlugins(() => [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ]),
+});
+
+export default config;
